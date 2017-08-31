55bcfa7517e98e73d4def993215566fdc7f3becb
uberraschung <matheus_garcias@hotmail.com>
2017-08-31 18:04:39 -0300

    Basic files

---
+++ main.lua
@@ -0,0 +1,15 @@
+--Global variables for easy and fast typing--------------------------------------------------------------------
+lg, lm, lk, lfs, lw, lt = love.graphics, love.mouse, love.keyboard, love.filesystem, love.window, love.timer
+---------------------------------------------------------------------------------------------------------------
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
