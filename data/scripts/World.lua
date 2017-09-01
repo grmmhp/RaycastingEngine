c6df4b82e4555311f15d8d92d71f6d7acc199e89
uberraschung <matheus_garcias@hotmail.com>
2017-09-01 13:04:07 -0300

    Metatables...

--- data/scripts/World.lua
+++ data/scripts/World.lua
@@ -1,18 +1,18 @@
 require"data.scripts.Map"
 
-World = {}
-World.mt = {__index=World}
+local World = {}
+local World_mt = {__index=World}
+setmetatable(World, {__call = function(t,map,entities) return World._new(t, map, entities) end})
 
-setmetatable(World,
-{__call = function(t,map,entities)
-  local w={
-    map=map or Map(),
-    entities=entities or {},
-  }
-
-  return setmetatable(World, World.mt)
-end})
+function World._new(t, map, entities)
+  local o = {}
+  o.map = map
+  o.entities = entities or {}
+  return setmetatable(o, World_mt)
+end
 
 function World:drawMiniMap(self)
   self.map:draw()
 end
+
+return World
--- main.lua
+++ main.lua
@@ -1,11 +1,12 @@
-require"data.scripts.World"
+local World = require "data.scripts.World"
 
 --Global variables for easy and fast typing--------------------------------------------------------------------
 lg, lm, lk, lfs, lw, lt = love.graphics, love.mouse, love.keyboard, love.filesystem, love.window, love.timer
 ---------------------------------------------------------------------------------------------------------------
 
 function love.load()
-  world=World()
+  world  = World(1, 2, 3)
+  world2 = World(4, 5, 6)
 end
 
 function love.update(dt)
@@ -13,5 +14,5 @@ function love.update(dt)
 end
 
 function love.draw()
-  world.map:drawMiniMap()
+  --world.map:drawMiniMap()
 end
